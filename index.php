8c161b8c4f1c0e615522d783be4aa2a26a5eb0b6
BaharJbn <86622863+BaharJbn@users.noreply.github.com>
2021-10-23 22:26:23 -0400

    registeration initial structure is added

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ require_once 'vendor/autoload.php';
 require_once 'init.php';
 require_once 'utils.php';
 // Define app routes
-require_once 'admin.php';
-require_once 'users.php';
+//require_once 'admin.php';
+require_once 'user.php';
 
 // Run app
 $app->run();
