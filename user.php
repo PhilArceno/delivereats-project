974dabdf083d235e3f8f78a66da7607ac25a780b
BaharJbn <86622863+BaharJbn@users.noreply.github.com>
2021-10-27 23:01:09 -0400

    uploadImage is fixed

--- user.php
+++ user.php
@@ -387,25 +387,64 @@ $app->post('/add-food', function ($request, $response, $args) use ($log) {
         }
         return TRUE;
     }
-
+    // description validation
     $result = verifyFoodDescription($description);
     if ($result !== TRUE) {
         $errorList[] = $result;
     }
 
-    $hasPhoto = false;
-    $mimeType = "";
+    function verifyUploadedFoodPhoto($photo, &$fileName) {
 
-    $uploadedFiles = $request->getUploadedFiles();
-    $uploadedImage = $uploadedFiles['image'];
+        if ($photo->getError() !== UPLOAD_ERR_OK) {
+            return "Error uploading photo " . $photo->getError();
+        }
+        if ($photo->getSize() > 1024*1024) { // 1MB max
+            return "File too big. 1MB max is allowed";
+        }
+        $info = getimagesize($photo->file);
+        if (!$info) {
+            return "File is not an image";
+        }
+        if ($info[0] < 200 || $info[0] > 1000 || $info[1] < 200 || $info[1] > 1000) {
+            return "Width and height must be within 200-1000 pixels range";
+        }
+        $ext = "";
+        switch ($info['mime']) {
+            case 'image/jpeg': $ext = "jpg"; break;
+            case 'image/gif': $ext = "gif"; break;
+            case 'image/png': $ext = "png"; break;
+            default:
+                return "Only JPG, GIF and PNG file types are allowed";
+        }
+        $filenameWithoutExtension = pathinfo($photo->getClientFilename(), PATHINFO_FILENAME);
+        // Note: keeping the original extension is dangerious and would allow for code injection - very dangerous
+        $sanitizedFileName = mb_ereg_replace('([^A-Za-z0-9_-])', '_', $filenameWithoutExtension);
+        $fileName = 'uploads/' . $sanitizedFileName . "." . $ext;
+        return TRUE;
+}
 
     // image validation
     $uploadedImage = $request->getUploadedFiles()['image'];
     $destImageFilePath = null;
-    $result = verifyUploadedPhoto($uploadedImage, $destImageFilePath);
+    $result = verifyUploadedFoodPhoto($uploadedImage, $destImageFilePath);
     if ($result !== TRUE) {
         $errorList []= $result;
     }
+
+
+    //$hasPhoto = false;
+    //$mimeType = "";
+
+    //$uploadedFiles = $request->getUploadedFiles();
+    //$uploadedImage = $uploadedFiles['image'];
+
+    // image validation
+    //$uploadedImage = $request->getUploadedFiles()['image'];
+    //$destImageFilePath = null;
+    //$result = verifyUploadedPhoto($uploadedImage, $destImageFilePath);
+    //if ($result !== TRUE) {
+    //    $errorList []= $result;
+    //}
 
     if ($errorList) {
         $valuesList = [
@@ -419,9 +458,9 @@ $app->post('/add-food', function ($request, $response, $args) use ($log) {
             'restaurant_id' => $restaurant_id,
         ];
         $uploadedImage->moveTo($destImageFilePath); // FIXME: check if it failed !
-        $valuesList['itemImagePath'] = $destImageFilePath;
+        $valuesList['imageFilePath'] = $destImageFilePath;
         DB::insert('food', $valuesList);
-
+ 
         return $this->view->render($response, "add-food-success.html.twig");
     }
 });
